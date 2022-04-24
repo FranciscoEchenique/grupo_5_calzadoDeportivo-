d8eca8887cd5110603c2e6560f544da1d8c78d37
FranciscoEchenique <echeniquefrancisco127@gmail.com>
2022-04-24 17:54:50 -0300

    Dinamizando el home

--- Entrega Sprint 2/controllers/mainController.js
+++ Entrega Sprint 2/controllers/mainController.js
@@ -1,8 +1,8 @@
 const home = (req, res) => {
-    res.render('home', {productos: productos});
+    res.render('home', {mejoresProductos: mejoresProductos, masComprado: masComprado, mejoresOfertas: mejoresOfertas});
 };
 
-const productos = [
+const mejoresProductos = [
     {
         nombre: "Nike Air Max 720",
         foto: "/images/nike-air-max-720.jpg",
@@ -22,6 +22,58 @@ const productos = [
         nombre: "Adidas Ozweego",
         foto: "images/adidas-ozweego.jpg",
         precio: "$11.000",
+    },
+]
+const masComprado = [
+    {
+        nombre: "Nike Air Max Se",
+        foto: "images/zapatillas-nike-air-max-se.jpg",
+        precio: "$18.500",
+    },
+    {
+        nombre: "Nike Air Dunk Low",
+        foto: "images/nike-dunk-low.jpg",
+        precio: "$14.800"
+    },
+    {
+        nombre: "Puma Mirage Tech",
+        foto: "images/puma-mirage-tech.jpg",
+        precio: "$10.000"
+    },
+    {
+        nombre: "Adidas Super Star",
+        foto: "images/adidas-superstar-bold.jpg",
+        precio: "$12.000",
+    },
+]
+const mejoresOfertas = [
+    {
+        nombre: "Nike Air Vapormax 360",
+        foto: "images/nike-air-vapormax-360.jpg",
+        precio: "$22.000",
+        precioTachado: "$35.000",
+        descuento: "30% OFF",
+    },
+    {
+        nombre: "Nike Air Force 1 Shadow",
+        foto: "images/nike-air-force-1-shadow.jpg",
+        precio: "$14.300",
+        precioTachado: "$18.300",
+        descuento: "20% OFF",
+    },
+    {
+        nombre: "Nike Air Max 97 Negras",
+        foto: "images/nike-air-max-97-negras.jpg",
+        precio: "$13.000",
+        precioTachado: "$17.000",
+        descuento: "15% OFF",
+    },
+    {
+        nombre: "Puma Wild Rider Rollin",
+        foto: "images/puma-wild-rider-rollin.jpg",
+        precio: "$11.000",
+        precioTachado: "$13.500",
+        descuento: "18% OFF",
     },
 ]
 

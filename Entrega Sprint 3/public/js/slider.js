8ae99bff07d96e25dca0112efc95724a304daa03
PaoloBotto <paolobotto1981@gmail.com>
2022-07-03 19:19:07 -0300

    agregando boton crear

--- Entrega Sprint 3/public/js/slider.js
+++ Entrega Sprint 3/public/js/slider.js
@@ -1,5 +1,12 @@
+window.addEventListener("load",function(e){
+ let clickiz = document.querySelector("#btn-left");
+   clickiz.addEventListener("click", function(){
+    console.log("click en boton izquierdo")
+    sliderSection1.style.
+   })
 
 
-console.log("probando slider");
 
 
+
+})

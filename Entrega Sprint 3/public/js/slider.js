fe7e99034d1c07bd4458a233f0b67851f56c006d
PaoloBotto <paolobotto1981@gmail.com>
2022-07-05 21:51:29 -0300

    creando carrousel de imagenes

--- Entrega Sprint 3/public/js/slider.js
+++ Entrega Sprint 3/public/js/slider.js
@@ -1,12 +1,31 @@
-window.addEventListener("load",function(e){
- let clickiz = document.querySelector("#btn-left");
-   clickiz.addEventListener("click", function(){
-    console.log("click en boton izquierdo")
-    sliderSection1.style.
-   })
+var imagenes = ["./images/c1.jpg","./images/c2.jpg","./images/c3.jpg","./images/c4.jpg"];
+let cont = 0; 
+window.addEventListener("load",function(){
+  document.addEventListener("click", e =>{
+    console.log("probando boton");
+    let atras = document.querySelector(".atras.botones");
+    let adelante = document.querySelector(".adelante.botones");
+    let img = document.querySelector("#imagen");
+    var tgt = e.target; 
 
+    if( tgt == atras){
+        if (cont > 0 ){
+            
+            img.src = imagenes [cont -1];
+            cont --;
+          }else{
+            img.src = imagenes [imagenes.length -1];
+            cont = imagenes.length - 1;
+          } 
+        }else if (tgt == adelante){
+          if (cont  < imagenes.length -1 ){
+           img.src = imagenes [cont + 1];
+           cont ++;
+          }else{
+           img.src = imagenes [0];
+           cont = 0;
+          }
+        }
+    });
 
-
-
-
-})
+});

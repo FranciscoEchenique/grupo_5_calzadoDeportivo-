466a555d5bf76f10e4930c71797b2188e1e4f077
PaoloBotto <paolobotto1981@gmail.com>
2022-07-26 22:38:40 -0300

    subiendo cambios sprint7

---
+++ Entrega Sprint 3/public/js/login.js
@@ -0,0 +1,30 @@
+window.addEventListener("load",function( ){
+   
+    let formularioLogin = document.querySelector("form.formulario");
+    let email = document.querySelector("#usuario");
+    let contraseña = document.querySelector("#clave");
+
+    
+    formularioLogin.addEventListener("submit", function(e){
+        console.log("capturado")
+        let errores = [];
+        if(email.value == ""){
+            errores.push("Ingresa tu email");
+        }
+        if (contraseña.value == ""){
+            errores.push("Ingresa tu contraseña");
+        }
+        if (errores.length > 0){
+            e.preventDefault();
+
+            let ulErrores = document.querySelector("div.errores");
+            for ( let i = 0; i < errores.length; i ++ ){
+                ulErrores.innerHTML += "<h4>" + errores[i] + "</h4>" 
+            }
+
+        }
+
+    })
+
+
+})

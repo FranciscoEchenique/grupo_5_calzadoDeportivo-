c7ad69e56f7f616bf1283a9d06e9dda00563016b
PaoloBotto <paolobotto1981@gmail.com>
2022-07-29 00:58:37 -0300

    modificando validaciones

--- Entrega Sprint 3/public/js/login.js
+++ Entrega Sprint 3/public/js/login.js
@@ -1,4 +1,4 @@
-window.addEventListener("load",function( ){
+window.onload =function(){
    
     let formularioLogin = document.querySelector("form.formulario");
     let email = document.querySelector("#usuario");
@@ -6,25 +6,27 @@ window.addEventListener("load",function( ){
 
     
     formularioLogin.addEventListener("submit", function(e){
+        
         console.log("capturado")
-        let errores = [];
+        let errores = {
+            email:"",
+            password:"",
+        }
+      
         if(email.value == ""){
-            errores.push("Ingresa tu email");
+            errores.email= "Ingresa tu email";
         }
         if (contraseña.value == ""){
-            errores.push("Ingresa tu contraseña");
-        }
-        if (errores.length > 0){
+            errores.password ="Ingresa tu contraseña";
             e.preventDefault();
-
-            let ulErrores = document.querySelector("div.errores");
-            for ( let i = 0; i < errores.length; i ++ ){
-                ulErrores.innerHTML += "<h4>" + errores[i] + "</h4>" 
-            }
-
         }
-
+       
+            let ulErrores = document.querySelector("div.errores-email");
+                ulErrores.innerHTML = "<h4>" + errores.email + "</h4>" 
+            let ulErrores2 = document.querySelector("div.errores-password");
+                ulErrores2.innerHTML = "<h4>" + errores.password + "</h4>"    
+       
+ 
     })
-
-
-})
+ 
+} 
--- Entrega Sprint 3/public/js/register.js
+++ Entrega Sprint 3/public/js/register.js
@@ -1,42 +1,57 @@
-window.addEventListener("load",function(event){
+window.onload=function( ){
    
     let formularioRegistro = document.querySelector("form.formulario-registro");
     let nombre = document.querySelector("#nombre");
     let apellido = document.querySelector("#apellido");
     let email = document.querySelector("#email");
     let password = document.querySelector("#password")
- 
-    
- 
-
+    let passwordConfirm= document.querySelector("#passwordConfirm")
     
     formularioRegistro.addEventListener("submit", function(e){
-            e.preventDefault();
+           
             console.log("capturando formulario");
-        let errores = [];
+        let errores = {
+            nombre:"",
+            apellido:"",
+            email:"",
+            password:"",
+            passwordConfirm:"",
+         
+        };
         if(nombre.value == ""){
-            errores.push("Ingresa tu nombre");
+            errores.nombre= "Ingresa tu nombre";
         }
         if(apellido.value == ""){
-                errores.push("Ingresa tu apellido");
+            errores.apellido= "Ingresa tu apellido";
         }            
         if(email.value == ""){
-            errores.push("Ingresa tu email");
+            errores.email="Ingresa tu email";
         }  
         if(password.value == ""){
-        errores.push("Ingresa una contraseña");
-        }  
-        if (errores.length > 0){
-        
-
-            let ulErrores = document.querySelector("div.errores");
-            for ( let i = 0; i < errores.length; i ++ ){
-                ulErrores.innerHTML += "<h4>" + errores[i] + "</h4>" 
-            }
-
+        errores.password="Ingresa una contraseña";
+        }
+        if(passwordConfirm.value == "" ){
+        errores.passwordConfirm="Reingresa tu contraseña";
+          e.preventDefault();
         }
 
+
+
+        
+           
+
+            let ulErrores = document.querySelector("div.errores-nombre");
+                ulErrores.innerHTML = "<h4>" + errores.nombre + "</h4>" 
+            let ulErrores1 = document.querySelector("div.errores-apellido");
+                ulErrores1.innerHTML = "<h4>" + errores.apellido + "</h4>" 
+            let ulErrores2 = document.querySelector("div.errores-email");
+                ulErrores2.innerHTML = "<h4>" + errores.email + "</h4>" 
+            let ulErrores3 = document.querySelector("div.errores-password");
+                ulErrores3.innerHTML = "<h4>" + errores.password + "</h4>"
+            let ulErrores4 = document.querySelector("div.errores-passwordConfirm");
+                ulErrores4.innerHTML = "<h4>" + errores.passwordConfirm + "</h4>"
+
+
     })
+}
 
-
-})

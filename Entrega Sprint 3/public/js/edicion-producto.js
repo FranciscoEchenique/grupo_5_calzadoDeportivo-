21ef6504d286468987deb72dba4e76e444c2d6dd
Christian-Leonel <christiandiazluna@gmail.com>
2022-08-01 15:11:08 -0300

    Actualizacion testeo validacion

--- Entrega Sprint 3/public/js/edicion-producto.js
+++ Entrega Sprint 3/public/js/edicion-producto.js
@@ -1,5 +1,5 @@
 
-// Testeo
+// Testeo 1
 
 // window.addEventListener("load",function( ){
 
@@ -37,36 +37,134 @@
 
 
 
-window.addEventListener("load",function( ){
 
 
-    let formularioEdicion = document.querySelector("form.formulario-creacion-productos");
-    const nombre = document.querySelector("#nombre");
-    const descripcion = document.querySelector("#descripcion");
 
 
-    formularioEdicion.addEventListener("submit", function(e){
-        console.log("capturado")
 
-        e.preventDefault();
-
-        let errores = [];
-        if(nombre.value == ""){
-            errores.push("Ingresa un nombre");
-        }
-        if (descripcion.value == ""){
-            errores.push("Ingresa una descripcion");
-        }
-        if (errores.length > 0){
-
-            let ulErrores = document.querySelector("div.errores ul");
-            for ( let i = 0; i < errores.length; i ++ ){
-                ulErrores.innerHTML += "<li>" + errores[i] + "</li>" 
-            }
-
-        }
-
-    })
+// ----------------------------------------------------------------------------------
 
 
+
+
+// Testeo 2
+
+// window.addEventListener("load",function( ){
+
+
+//     let formularioEdicion = document.querySelector("form.formulario-creacion-productos");
+//     const nombre = document.querySelector("#nombre");
+//     const descripcion = document.querySelector("#descripcion");
+
+
+//     formularioEdicion.addEventListener("submit", function(e){
+//         console.log("capturado")
+
+//         e.preventDefault();
+
+//         let errores = [];
+//         if(nombre.value == ""){
+//             errores.push("Ingresa un nombre");
+//         }
+//         if (descripcion.value == ""){
+//             errores.push("Ingresa una descripcion");
+//         }
+//         if (errores.length > 0){
+
+//             let ulErrores = document.querySelector("div.errores ul");
+//             for ( let i = 0; i < errores.length; i ++ ){
+//                 ulErrores.innerHTML += "<li>" + errores[i] + "</li>" 
+//             }
+
+//         }
+
+//     })
+
+
+// })
+
+
+
+
+
+
+
+
+
+// ------------------------------------------------------------------------------------
+
+
+
+
+
+
+// Testeo 3
+
+
+// window.onload = function () {
+
+//     let formularioEdicion = document.querySelector("form.formulario-creacion-productos");
+//     let nombre = document.querySelector("#nombre");
+//     let descripcion = document.querySelector("#descripcion");
+
+
+//     formularioEdicion.addEventListener("submit", function (e) {
+
+//         console.log("capturado")
+//         let errores = {
+//             nombre: "",
+//             descripcion: "",
+//         }
+
+//         if (nombre.value == "") {
+//             errores.nombre = "Ingresa un nombre";
+//         }
+//         if (descripcion.value == "") {
+//             errores.descripcion = "Ingresa una descripcion al producto";
+//             e.preventDefault();
+//         }
+
+//         let ulErrores = document.querySelector("div.errores-nombre");
+//         ulErrores.innerHTML = "<h4>" + errores.nombre + "</h4>"
+//         let ulErrores2 = document.querySelector("div.errores-descripcion");
+//         ulErrores2.innerHTML = "<h4>" + errores.descripcion + "</h4>"
+
+
+//     })
+
+// } 
+
+
+
+
+
+// Testeo 4
+
+
+
+
+const nombre = document.getElementById("nombre")
+const descripcion = document.getElementById("descripcion")
+const form = document.getElementById("form")
+const parrafo = document.getElementById("warnings")
+
+form.addEventListener("submit", e=>{
+    e.preventDefault()
+    let warnings = ""
+    let entrar = false
+    parrafo.innerHTML = ""
+    if(nombre.value.length <5){
+        warnings += `El nombre no es valido <br>`
+        entrar = true
+    }
+    if(descripcion.value.length < 20){
+        warnings += `La descripcion no es valida <br>`
+        entrar = true
+    }
+
+    if(entrar){
+        parrafo.innerHTML = warnings
+    }else{
+        parrafo.innerHTML = "Enviado"
+    }
 })
